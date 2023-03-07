d491cf058b98cd3e4787a2674885aff37e578e0a
Michael Hla <YOUR@EMAIL>
2023-03-07 17:23:58 -0500

    connections good

--- machine.py
+++ machine.py
@@ -21,18 +21,20 @@ def serverthread(IP, port):
         conn, addr = server.accept()
         print(addr[0] + " connected")
         # creates an individual thread for each machine that connects
-        start_new_thread(msg_listen, (conn))
+        start_new_thread(msg_listen, (conn,))
 
 
 def msg_listen(conn, IP=None, port=None):
     # this is where a machine will listen when it recognizes a connection
-    # either gets a conn from the server for listening or connects to existing server
+    # either gets a conn from machine server for listening or connects to existing server
     if conn is None:
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect(IP, port)
+        conn.connect((IP, port))
+
     connlock.acquire(timeout=10)
     gconn_list.append(conn)
     connlock.release()
+
     while True:
         try:
             msg = conn.recv(2048)
@@ -49,7 +51,7 @@ def msg_listen(conn, IP=None, port=None):
 
 
 def running_machine(rate):
-    time.sleep(rate)
+    time.sleep(1/rate)
     clock = [0, 0, 0]
     while True:
         # check our message queue
@@ -61,11 +63,8 @@ def running_machine(rate):
         qlock.release()
 
         # check connlist for updates
-        connlock.acquire(timeout=10)
-        conn_list = gconn_list
-        connlock.release()
 
-        time.sleep(rate)
+        time.sleep(1/rate)
         # act accordingly to the message
         if msg:
             clock = msg
@@ -75,10 +74,17 @@ def running_machine(rate):
                 # NOT SURE if able to send message when separate thread connected
                 # TODO figure out how to send to certain connections
                 # maybe have to open connections in machine thread, then open listen threads with that connection passed in
-                conn_list[op].send(clock[0])
+                connlock.acquire(timeout=10)
+                if len(gconn_list) > 1:
+                    conn = gconn_list[op]
+                    conn.send((1).to_bytes(1, "big"))
+                connlock.release()
             elif op == 3:
-                for conn in conn_list:
-                    conn.send(clock)
+                connlock.acquire(timeout=10)
+                for conn in gconn_list:
+                    conn.send((1).to_bytes(1, "big"))
+                connlock.release()
+
             else:
                 clock[0] += 1
 
@@ -87,16 +93,20 @@ if __name__ == '__main__':
     this_IP = '10.250.189.78'
     thread_list = []
     # usage: python3 machine.py rate listenPORT targetIP targetPORT...
+    # start machines in sequence
 
     # connect to all existing machines
-    for i in range(4, sys.argv, 2):
-        thread_list.append(threading.Thread(
-            target=msg_listen, args=(None, sys.argv[i], sys.argv[i+1])))
-        thread_list[i].start()
+    if len(sys.argv) > 3:
+        for i in range(3, len(sys.argv), 2):
+            thread_list.append(threading.Thread(
+                target=msg_listen, args=(None, sys.argv[i], int(sys.argv[i+1]))))
+            thread_list[i-3].start()
 
     # start machine thread
     threading.Thread(target=running_machine,
-                     args=(10)).start()  # change rate as arg
+                     args=(int(sys.argv[1]),)).start()  # change rate as arg
 
     # then start its own thread to listen for future connections
-    threading.Thread(target=serverthread, args=(this_IP, sys.argv[3])).start()
+    threading.Thread(target=serverthread, args=(
+        this_IP, int(sys.argv[2]))).start()
+    print("init complete")
